ae1356b36e12ff4366ca3311258f9bbf51331683
Aahan <125281835+Aahan22001IIITD@users.noreply.github.com>
2024-10-22 12:22:56 +0530

    Update server.c

--- q2/c/server.c
+++ q2/c/server.c
@@ -1,21 +1,32 @@
-#include <iostream>
+#include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <errno.h>  
+
 #include <sys/select.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
 #define PORT 8080
 #define MAX_CLIENTS 10
 #define BUFFER_SIZE 1024
 
-// Handle the client request in the select loop without using threads
-void handle_client(int sock) {
+// Structure for client details
+typedef struct {
+    int socket;
+} client_t;
+
+// Function to handle the client connection in a separate thread
+void *handle_client(void *arg) {
+    client_t *cli = (client_t *)arg;
+    int sock = cli->socket;
+    free(cli);  // Free the client structure after receiving it
+
     char buffer[BUFFER_SIZE];
     char response[BUFFER_SIZE * 2]; 
     memset(response, 0, sizeof(response)); 
@@ -26,7 +37,7 @@ void handle_client(int sock) {
     if (fp == NULL) {
         perror("Failed to run command");
         close(sock);
-        return;
+        return NULL;
     }
 
     // Read command output and append to response
@@ -37,10 +48,8 @@ void handle_client(int sock) {
 
     // Send the response to the client
     send(sock, response, strlen(response), 0);
-    std::cout << "Response sent to socket " << sock << std::endl;
-
-    // Close the connection
-    close(sock);
+    close(sock);  // Close the connection
+    return NULL;
 }
 
 int main() {
@@ -48,6 +57,7 @@ int main() {
     struct sockaddr_in address;
     fd_set readfds;
     char buffer[BUFFER_SIZE];
+    pthread_t thread_id;
 
     // Initialize client sockets
     for (int i = 0; i < MAX_CLIENTS; i++) {
@@ -76,25 +86,21 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Listen for incoming connections
     if (listen(server_fd, 3) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "Listening on port " << PORT << std::endl;
+    printf("Listening on port %d\n", PORT);
 
     int addrlen = sizeof(address);
 
-    while (true) {
-        // Clear the socket set
+    while (1) {
         FD_ZERO(&readfds);
 
-        // Add server socket to set
         FD_SET(server_fd, &readfds);
         max_sd = server_fd;
 
-        // Add client sockets to set
         for (int i = 0; i < MAX_CLIENTS; i++) {
             sd = client_sockets[i];
             if (sd > 0) {
@@ -105,42 +111,51 @@ int main() {
             }
         }
 
-        // Wait for activity on any of the sockets
         int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
         if ((activity < 0) && (errno != EINTR)) {
             perror("Select error");
         }
 
-        // If something happened on the server socket, it's an incoming connection
         if (FD_ISSET(server_fd, &readfds)) {
             if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
                 perror("Accept failed");
                 exit(EXIT_FAILURE);
             }
 
-            std::cout << "New connection, socket fd is " << new_socket << std::endl;
+            printf("New connection, socket fd is %d\n", new_socket);
 
             // Add new socket to the client_sockets array
             for (int i = 0; i < MAX_CLIENTS; i++) {
                 if (client_sockets[i] == 0) {
                     client_sockets[i] = new_socket;
-                    std::cout << "Adding to list of sockets as " << i << std::endl;
+                    printf("Adding to list of sockets as %d\n", i);
                     break;
                 }
             }
+
+            // Create a thread for the new connection
+            client_t *cli = (client_t *)malloc(sizeof(client_t));
+            cli->socket = new_socket;
+            if (pthread_create(&thread_id, NULL, handle_client, (void *)cli) != 0) {
+                perror("Failed to create thread");
+            }
+            pthread_detach(thread_id);  // Detach the thread to clean up automatically
         }
 
         // Check all client sockets
         for (int i = 0; i < MAX_CLIENTS; i++) {
             sd = client_sockets[i];
-
             if (FD_ISSET(sd, &readfds)) {
-                // Handle client request and send the CPU process response
-                std::cout << "Processing request on socket " << sd << std::endl;
-                handle_client(sd);
+                // If a socket is ready to be read, handle it in a thread
+                int valread = read(sd, buffer, BUFFER_SIZE);
+                if (valread == 0) {
+                    // Client disconnected
+                    getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+                    printf("Host disconnected, IP %s, port %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
-                // After handling, mark the socket as closed
-                client_sockets[i] = 0;
+                    close(sd);
+                    client_sockets[i] = 0;
+                }
             }
         }
     }
